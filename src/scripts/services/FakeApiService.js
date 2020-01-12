4704b59f48d526b2cb2dcb1d144fdbbbffb66e89
Eugene <gk16121983@gmail.com>
2020-01-12 21:02:32 +0200

    refactor FakeApiService.js

--- src/scripts/services/FakeApiService.js
+++ src/scripts/services/FakeApiService.js
@@ -3,12 +3,50 @@ class FakeApiService {
         this._apiBase = 'http://localhost:3000/api'
     }
 
-    async signUp() {}
-    async login() {}
+    async signUp(user) {
+        const signUp = await fetch(`${this._apiBase}/users`, {
+            method: 'POST',
+            body: JSON.stringify({
+                email: user.email,
+                password: user.password,
+                name: user.name
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        return signUp
+    }
+
+    async login(user) {
+        const login = await fetch(`${this._apiBase}/users/login`, {
+            method: 'POST',
+            body: JSON.stringify({
+                email: user.email,
+                password: user.password
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        return login
+    }
+
     async resetPassword() {}
     async startAThread() {}
-    async getAllThreads() {}
+
+    async getAllThreads(token) {
+        const threads = await fetch(`${this._apiBase}/threads`, {
+            headers: {
+                Authorization: `${token}`
+            }
+        })
+        return threads
+    }
     async sendMessage() {}
     async getThreadMessages() {}
 
 }
+
+const fakeApiService = new FakeApiService()
+export default fakeApiService
