38c92557e00a6d0ee04be5ec796b032cf6cc1c60
Eugene <gk16121983@gmail.com>
2020-01-12 19:11:27 +0200

    fix main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,5 @@
 import headerCmp from "./components/HeaderComponent.js";
-// import asideCmp from "./components/AsideComponent.js";
-// import mainCmp from "./components/MainComponent.js";
 import middleCmp from "./components/MiddleComponent.js";
-// const middleCmp = document.createElement('div')
-// middleCmp.classList.add('middle')
-// middleCmp.append(asideCmp.render())
-// middleCmp.append(mainCmp.render())
 
 const components = [headerCmp,middleCmp]
-
 export default components
