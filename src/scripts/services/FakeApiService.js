00d3c1f1fb1ce80a0f7dfa13c6e485295fffb8c5
Eugene <gk16121983@gmail.com>
2020-01-12 19:30:44 +0200

    feature FakeApiService.js

---
+++ src/scripts/services/FakeApiService.js
@@ -0,0 +1,14 @@
+class FakeApiService {
+    constructor() {
+        this._apiBase = 'http://localhost:3000/api'
+    }
+
+    async signUp() {}
+    async login() {}
+    async resetPassword() {}
+    async startAThread() {}
+    async getAllThreads() {}
+    async sendMessage() {}
+    async getThreadMessages() {}
+
+}
