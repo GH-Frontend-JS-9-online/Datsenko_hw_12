30a94c5ee86826dd71efdd7ef750a92e150aea9d
Eugene <gk16121983@gmail.com>
2020-01-12 23:19:17 +0200

    refactor AsideComponent.js

--- src/scripts/components/AsideComponent.js
+++ src/scripts/components/AsideComponent.js
@@ -3,39 +3,21 @@ import HTML from './HTML.js'
 class AsideComponent {
     constructor() {}
     render() {
+        const dataImg = ['home', 'menu', 'trending-up', 'email', 'account-multiple']
         let asideComponent = HTML(`
             <aside class="aside">
                 <nav class="aside__nav">
                     <ul class="aside__nav__menu">
-                        <li class="aside__nav__menu__item">
-                            <a href="#" class="aside__nav__menu__link">
-                                <img src="./assets/images/aside/home.png" alt="home">
-                            </a>
-                        </li>
-                        <li class="aside__nav__menu__item">
-                            <a href="#" class="aside__nav__menu__link">
-                                <img src="./assets/images/aside/menu.png" alt="menu">
-                            </a>
-                        </li>
-                        <li class="aside__nav__menu__item">
-                            <a href="#" class="aside__nav__menu__link">
-                                <img src="./assets/images/aside/trending-up.png" alt="trending-up">
-                            </a>
-                        </li>
-                        <li class="aside__nav__menu__item">
-                            <a href="#" class="aside__nav__menu__link">
-                                <img src="./assets/images/aside/email.png" alt="email">
-                            </a>
-                        </li>
-                        <li class="aside__nav__menu__item">
-                            <a href="#" class="aside__nav__menu__link">
-                                <img src="./assets/images/aside/account-multiple.png" alt="account-multiple">
-                            </a>
-                        </li>
+                        ${dataImg.map(item => `
+                             <li class="aside__nav__menu__item">
+                                <a href="#">
+                                    <img src="./assets/images/aside/${item}.png" alt="${item}">
+                                </a>
+                            </li>                         
+                        `).join('')}
                     </ul>
                 </nav>
-            </aside>
-        
+            </aside>        
         `)
         return asideComponent
     }
@@ -43,3 +25,4 @@ class AsideComponent {
 
 const asideCmp = new AsideComponent()
 export default  asideCmp
+
