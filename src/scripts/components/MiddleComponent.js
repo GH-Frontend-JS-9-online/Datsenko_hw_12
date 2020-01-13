f25374ec2664b4628bdcb8681aadcad909bad33e
Eugene <gk16121983@gmail.com>
2020-01-13 17:13:46 +0200

    refactor MiddleComponent.js

--- src/scripts/components/MiddleComponent.js
+++ src/scripts/components/MiddleComponent.js
@@ -4,18 +4,15 @@ import mainCmp from "./MainComponent.js";
 
 class MiddleComponent {
     constructor() {}
-
     render() {
         let middleComponent = HTML(`
             <div class="middle"></div>      
         
         `)
-
         middleComponent.append(asideCmp.render())
         middleComponent.append(mainCmp.render())
         return middleComponent
     }
 }
-
 const middleCmp = new MiddleComponent()
 export default  middleCmp
