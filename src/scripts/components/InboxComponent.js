2748041a478b13b45f336eb381e53310ce064511
Eugene <gk16121983@gmail.com>
2020-01-16 19:18:16 +0200

    fix InboxComponent.js

--- src/scripts/components/InboxComponent.js
+++ src/scripts/components/InboxComponent.js
@@ -1,31 +1,4 @@
-// import HTML from './HTML.js'
-// //
-// // class InboxComponent {
-// //     constructor() {}
-// //     render() {
-// //         let inboxComponent = HTML(`
-// //             <div class="main-message-container__left-aside inbox">
-// //                <div class="conversations">
-// //                    <div class="conversation">
-// //
-// //                    </div>
-// //                    <div class="new-conversation">
-// //                        <button class="new-conversation__btn">
-// //                            <img src="./assets/images/main/plus.png" alt="plus">
-// //                            New coversation
-// //                        </button>
-// //                    </div>
-// //                </div>
-// //             </div>
-// //         `)
-// //         return inboxComponent
-// //     }
-// // }
-// // const inboxCmp = new InboxComponent()
-// // export default inboxCmp
-
 import HTML from "./HTML.js";
-
 
 class InboxComponent {
     constructor() {
