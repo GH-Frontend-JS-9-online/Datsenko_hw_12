c4bd67ec08805689287041ed6014a4c8581b4bb3
Eugene <gk16121983@gmail.com>
2020-01-16 14:02:20 +0200

    refactor hw12 global

--- src/scripts/components/InboxComponent.js
+++ src/scripts/components/InboxComponent.js
@@ -1,25 +1,76 @@
-import HTML from './HTML.js'
+// import HTML from './HTML.js'
+// //
+// // class InboxComponent {
+// //     constructor() {}
+// //     render() {
+// //         let inboxComponent = HTML(`
+// //             <div class="main-message-container__left-aside inbox">
+// //                <div class="conversations">
+// //                    <div class="conversation">
+// //
+// //                    </div>
+// //                    <div class="new-conversation">
+// //                        <button class="new-conversation__btn">
+// //                            <img src="./assets/images/main/plus.png" alt="plus">
+// //                            New coversation
+// //                        </button>
+// //                    </div>
+// //                </div>
+// //             </div>
+// //         `)
+// //         return inboxComponent
+// //     }
+// // }
+// // const inboxCmp = new InboxComponent()
+// // export default inboxCmp
+
+import HTML from "./HTML.js";
+
 
 class InboxComponent {
-    constructor() {}
+    constructor() {
+    }
+
     render() {
         let inboxComponent = HTML(`
-            <div class="main-message-container__left-aside inbox">
-               <div class="conversations">
-                   <div class="conversation">
-
-                   </div>
-                   <div class="new-conversation">
-                       <button class="new-conversation__btn">
-                           <img src="./assets/images/main/plus.png" alt="plus">
-                           New coversation
-                       </button>
-                   </div>
-               </div>
-            </div>
-        `)
+        <div class="inbox">             
+                <div class="messages">
+                    <div class="person">
+                        <div>
+                            <img src="./assets/images/main/Elipsa_1_kopia.png" alt="">
+                            <h2>Michelle Stewart</h2>
+                        </div>
+                        <span>Today, 5:32 PM</span>
+                    </div>
+                     <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusm.
+                    </p>
+                </div>
+                <div class="messages">
+                    <div class="person">
+                        <div>
+                            <img src="./assets/images/main/Elipsa_1_kopia.png" alt="">
+                            <h2>Michelle Stewart</h2>
+                        </div>
+                        <span>Today, 5:32 PM</span>
+                    </div>
+                     <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusm.
+                    </p>
+                </div> 
+                <div class="new-conversation">
+                    <button type="button" class="new-conversation__btn">
+                        <img src="./assets/images/main/plus.png" alt="">
+                        New coversation
+                    </button>
+                </div>       
+        </div>
+                
+       
+    `)
         return inboxComponent
     }
 }
+
 const inboxCmp = new InboxComponent()
 export default inboxCmp
--- src/scripts/services/FakeApiService.js
+++ src/scripts/services/FakeApiService.js
@@ -2,7 +2,7 @@ import Storage from "./Storage.js";
 
 class FakeApiService {
     constructor() {
-        this._apiBase = 'http://localhost:3000/api'
+        this._apiBase = 'https://geekhub-frontend-js-9.herokuapp.com/api'
     }
 
     async signUp(user) {
@@ -23,140 +23,150 @@ class FakeApiService {
     async login(user) {
         const login = await fetch(`${this._apiBase}/users/login`, {
             method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
             body: JSON.stringify({
                 email: user.email,
                 password: user.password
             }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
         })
         return login
     }
 
-    async resetPassword(user) {
-        this.login(user)
-            .then(response => {
-                Storage.setData('token', response.headers.get('x-auth-token'))
-            })
-            .catch(error => console.log(error))
-        let token = Storage.getData('token')
-        const reset = await fetch(`${this._apiBase}/users/reset_password`, {
-            method: 'POST',
-            body: JSON.stringify({
-                password: user.password,
-                confirmationPassword: user.confirmationPassword,
-                email: user.email
-            }),
+    async getCurrentUser() {
+        const currentUser = fetch(`${this._apiBase}/users/current`, {
+            method: 'GET',
             headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'x-access-token': token
+                'x-access-token': 'null'
             }
         })
-        Storage.removeData()
-        return reset
-    }
-    async startAThread(user) {
-        this.login(user)
-            .then(response => {
-            Storage.setData('token', response.headers.get('x-auth-token'))
-        })
-        .catch(error => console.log(error))
-        this.login(user)
-            .then(response => response.json())
-            .then(response => {
-                Storage.setData('id', response._id)
-            })
-            .catch(error => console.log(error))
-        let token = Storage.getData('token')
-        let id = Storage.getData('id')
-        const thread = await fetch(`${this._apiBase}/threads`, {
-            method: 'POST',
-            body: JSON.stringify({
-                user: {
-                    _id: id
-                }
-            }),
-            headers: {
-                'Authorization': token,
-                'Content-Type': 'application/json'
-            }
-        })
-        Storage.removeData()
-        return thread
+        return currentUser
     }
 
-    async getAllThreads(user) {
-        this.login(user)
-            .then(response => {
-                Storage.setData('token', response.headers.get('x-auth-token'))
-            })
-            .catch(error => console.log(error))
-        let token = Storage.getData('token')
-        const threads = await fetch(`${this._apiBase}/threads`, {
-            method: 'GET',
-            headers: {
-                'Authorization': token
-            }
-        })
-        Storage.removeData()
-        return threads
-    }
-    async sendMessage(user, message) {
-        this.login(user)
-            .then(response => {
-                Storage.setData('token', response.headers.get('x-auth-token'))
-            })
-            .catch(error => console.log(error))
-        this.login(user)
-            .then(response => response.json())
-            .then(response => {
-                Storage.setData('id', response._id)
-            })
-            .catch(error => console.log(error))
-        let token = Storage.getData('token')
-        let id = Storage.getData('id')
-        const send = fetch(`${this._apiBase}/threads/messages`, {
-            method: 'POST',
-            body: JSON.stringify({
-                thread: {
-                    _id: id
-                },
-                message: {
-                    body: message
-                }
-            }),
-            headers: {
-                'Authorization': token,
-                'Content-Type': 'application/json'
-            }
-        })
-        Storage.removeData()
-        return send
-    }
-    async getThreadMessages(user) {
-        this.login(user)
-            .then(response => {
-                Storage.setData('token', response.headers.get('x-auth-token'))
-            })
-            .catch(error => console.log(error))
-        this.login(user)
-            .then(response => response.json())
-            .then(response => {
-                Storage.setData('id', response._id)
-            })
-            .catch(error => console.log(error))
-        let token = Storage.getData('token')
-        let id = Storage.getData('id')
-        const threadsMessages = fetch(`${this._apiBase}/threads/?${id}`, {
-            method: 'GET',
-            headers: {
-                'Authorization': token
-            }
-        })
-        Storage.removeData()
-        return threadsMessages
-    }
+    // async resetPassword(user) {
+    //     this.login(user)
+    //         .then(response => {
+    //             Storage.setData('token', response.headers.get('x-auth-token'))
+    //         })
+    //         .catch(error => console.log(error))
+    //     let token = Storage.getData('token')
+    //     const reset = await fetch(`${this._apiBase}/users/reset_password`, {
+    //         method: 'POST',
+    //         body: JSON.stringify({
+    //             password: user.password,
+    //             confirmationPassword: user.confirmationPassword,
+    //             email: user.email
+    //         }),
+    //         headers: {
+    //             'Content-Type': 'application/x-www-form-urlencoded',
+    //             'x-access-token': token
+    //         }
+    //     })
+    //     Storage.removeData()
+    //     return reset
+    // }
+    // async startAThread(user) {
+    //     this.login(user)
+    //         .then(response => {
+    //         Storage.setData('token', response.headers.get('x-auth-token'))
+    //     })
+    //     .catch(error => console.log(error))
+    //     this.login(user)
+    //         .then(response => response.json())
+    //         .then(response => {
+    //             Storage.setData('id', response._id)
+    //         })
+    //         .catch(error => console.log(error))
+    //     let token = Storage.getData('token')
+    //     let id = Storage.getData('id')
+    //     const thread = await fetch(`${this._apiBase}/threads`, {
+    //         method: 'POST',
+    //         body: JSON.stringify({
+    //             user: {
+    //                 _id: id
+    //             }
+    //         }),
+    //         headers: {
+    //             'Authorization': token,
+    //             'Content-Type': 'application/json'
+    //         }
+    //     })
+    //     Storage.removeData()
+    //     return thread
+    // }
+    //
+    // async getAllThreads(user) {
+    //     this.login(user)
+    //         .then(response => {
+    //             Storage.setData('token', response.headers.get('x-auth-token'))
+    //         })
+    //         .catch(error => console.log(error))
+    //     let token = Storage.getData('token')
+    //     const threads = await fetch(`${this._apiBase}/threads`, {
+    //         method: 'GET',
+    //         headers: {
+    //             'Authorization': token
+    //         }
+    //     })
+    //     Storage.removeData()
+    //     return threads
+    // }
+    // async sendMessage(user, message) {
+    //     this.login(user)
+    //         .then(response => {
+    //             Storage.setData('token', response.headers.get('x-auth-token'))
+    //         })
+    //         .catch(error => console.log(error))
+    //     this.login(user)
+    //         .then(response => response.json())
+    //         .then(response => {
+    //             Storage.setData('id', response._id)
+    //         })
+    //         .catch(error => console.log(error))
+    //     let token = Storage.getData('token')
+    //     let id = Storage.getData('id')
+    //     const send = fetch(`${this._apiBase}/threads/messages`, {
+    //         method: 'POST',
+    //         body: JSON.stringify({
+    //             thread: {
+    //                 _id: id
+    //             },
+    //             message: {
+    //                 body: message
+    //             }
+    //         }),
+    //         headers: {
+    //             'Authorization': token,
+    //             'Content-Type': 'application/json'
+    //         }
+    //     })
+    //     Storage.removeData()
+    //     return send
+    // }
+    // async getThreadMessages(user) {
+    //     this.login(user)
+    //         .then(response => {
+    //             Storage.setData('token', response.headers.get('x-auth-token'))
+    //         })
+    //         .catch(error => console.log(error))
+    //     this.login(user)
+    //         .then(response => response.json())
+    //         .then(response => {
+    //             Storage.setData('id', response._id)
+    //         })
+    //         .catch(error => console.log(error))
+    //     let token = Storage.getData('token')
+    //     let id = Storage.getData('id')
+    //     const threadsMessages = fetch(`${this._apiBase}/threads/?${id}`, {
+    //         method: 'GET',
+    //         headers: {
+    //             'Authorization': token
+    //         }
+    //     })
+    //     Storage.removeData()
+    //     return threadsMessages
+    // }
 }
 
 const fakeApiService = new FakeApiService()
