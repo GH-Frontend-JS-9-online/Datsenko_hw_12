d26c5671f49c45e208ab2e890609e188c737ce28
Eugene <gk16121983@gmail.com>
2020-01-16 17:40:58 +0200

    refactor InboxComponent.js

--- src/scripts/components/InboxComponent.js
+++ src/scripts/components/InboxComponent.js
@@ -32,32 +32,23 @@ class InboxComponent {
     }
 
     render() {
+        const personData = [{photo: 'Elipsa_1_kopia', name: 'Michelle Stewart', date: '5:32', message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusm.'}]
         let inboxComponent = HTML(`
-        <div class="inbox">             
-                <div class="messages">
+        <div class="inbox"> 
+                ${personData.map(({photo, name, date, message}) => `
+                    <div class="messages">
                     <div class="person">
                         <div>
-                            <img src="./assets/images/main/Elipsa_1_kopia.png" alt="">
-                            <h2>Michelle Stewart</h2>
+                            <img src="./assets/images/main/${photo}.png" alt="">
+                            <h2>${name}</h2>
                         </div>
-                        <span>Today, 5:32 PM</span>
+                        <span>Today, ${date} PM</span>
                     </div>
                      <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusm.
+                       ${message}
                     </p>
-                </div>
-                <div class="messages">
-                    <div class="person">
-                        <div>
-                            <img src="./assets/images/main/Elipsa_1_kopia.png" alt="">
-                            <h2>Michelle Stewart</h2>
-                        </div>
-                        <span>Today, 5:32 PM</span>
-                    </div>
-                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusm.
-                    </p>
-                </div> 
+                </div>             
+                `).join('')}                   
                 <div class="new-conversation">
                     <button type="button" class="new-conversation__btn">
                         <img src="./assets/images/main/plus.png" alt="">
