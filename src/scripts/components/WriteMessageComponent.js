0d7233fba8b729f42c0d1ae614619f70d73f0009
Eugene <gk16121983@gmail.com>
2020-01-17 22:46:34 +0200

    refactor WriteMessageComponent.js

--- src/scripts/components/WriteMessageComponent.js
+++ src/scripts/components/WriteMessageComponent.js
@@ -7,12 +7,13 @@ class WriteMessageComponent {
         let writeMessageComponent = HTML(`
             <form action="#" class="write-message">
                 <input type="text" name="write-message__text" placeholder="Write a message" required="required" pattern=".*\\S.*">
-                <button type="button" class="write-message__btn"><img src="./assets/images/main/attachment.png" alt="attachment"></button>
+                <button type="submit" class="write-message__btn"><img src="./assets/images/main/attachment.png" alt="attachment"></button>
             </form>
         `)
         const messageText = writeMessageComponent.querySelector('input[name="write-message__text"]')
         const writeMessageBtn = writeMessageComponent.querySelector('.write-message__btn')
-        writeMessageBtn.addEventListener('click', () => {
+        writeMessageBtn.addEventListener('click', (event) => {
+            event.preventDefault()
             if (messageText.value.trim() !== '') {
                 fakeApiService.sendMessage(user, messageText.value.trim())
                 .then(response => response.json())
