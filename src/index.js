9314cfdce08a87264e3d6113049fb1ce557b89fe
Eugene <gk16121983@gmail.com>
2020-01-19 15:11:17 +0200

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -5,5 +5,46 @@ import components from "./scripts/main.js";
 const app = document.getElementById('app')
 components.map(component => app.append(component.render()))
 
+import fakeApiService from "./scripts/services/FakeApiService.js";
 
 
+fakeApiService.signUp(user)
+    .then(response => response.json())
+    .then(data => console.table(data))
+    .catch(error => console.error(error))
+
+fakeApiService.login(user)
+    .then(response => response.json())
+    .then(data => console.table(data))
+    .catch(error => console.error(error))
+
+
+fakeApiService.resetPassword(user)
+    .then(response => response.json())
+    .then(data => console.table(data))
+    .catch(error => console.error(error))
+
+fakeApiService.getCurrentUser(user)
+    .then(response => response.json())
+    .then(data => console.table(data))
+    .catch(error => console.error(error))
+
+fakeApiService.getAllUsers(user)
+    .then(response => response.json())
+    .then(data => console.table(data))
+    .catch(error => console.error(error))
+
+fakeApiService.retrieveAllThreads(user)
+    .then(response => response.json())
+    .then(response => console.table(response))
+    .catch(error => console.error(error))
+
+fakeApiService.createThread(user)
+    .then(response => response.json())
+    .then(response => console.table(response))
+    .catch(error => console.error(error))
+
+fakeApiService.sendMessage(user, 'Hello World !!!')
+    .then(response => response.json())
+    .then(data => console.table(data))
+    .catch(error => console.error(error))
