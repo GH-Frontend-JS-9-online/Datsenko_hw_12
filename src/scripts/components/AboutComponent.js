500024c4a331fa658c33919e0a45ddadec949d25
Eugene <gk16121983@gmail.com>
2020-01-23 20:56:18 +0200

    refactor AboutComponent.js

--- src/scripts/components/AboutComponent.js
+++ src/scripts/components/AboutComponent.js
@@ -1,24 +1,23 @@
 import HTML from "./HTML.js";
+import fakeApiService from "../services/FakeApiService";
+import Storage from "../services/Storage.js";
 
 class AboutComponent {
     constructor() {}
-    render() {
-        let userData = [{_id: "5e13095244e24582b460517c",
-            name: "Michelle Stewart",
-            password: "$2b$10$JzoNeNEasPYqjMIBKsUNq.GrvtcBs9pIpG.DsMbdRYSkadjYBnHaK",
-            email: "michelle@geekhub.ck.ua",
-            position: "UX/UI Designer",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            phone: "+48 500 400 300",
-            address: "65 Lorem St, Warshaw, PL",
-            organization: "GeekHub Corp"}]
+    render(user) {
+        fakeApiService.login(user)
+            .then(response => response.json())
+            .then(data => Storage.setData('user', data))
+            .catch(error => console.error(error))
+        let userInfo = [Storage.getData('user')]
+        Storage.removeData()
         let aboutComponent = HTML(`
             <div class="about">
                 <div class="user-photo">
                     <img src="./assets/images/main/online.png" alt="online">
                     <img src="./assets/images/main/ellipse.png" alt="photo">
                 </div>
-                ${userData.map(({name, email, position, description, phone, address, organization}) => `
+                ${userInfo.map(({name, email, position, description, phone, address, organization}) => `
                     <div class="user-data">
                     <h2>${name}</h2>
                     <h3>${position}</h3>
