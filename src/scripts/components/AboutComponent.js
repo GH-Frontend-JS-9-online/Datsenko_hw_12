7629afd2c2a00743d8412fc4dc62cb088b04e9d6
Eugene <gk16121983@gmail.com>
2020-01-26 14:36:51 +0200

    refactor AboutComponent.js

--- src/scripts/components/AboutComponent.js
+++ src/scripts/components/AboutComponent.js
@@ -1,46 +1,44 @@
 import HTML from "./HTML.js";
-import fakeApiService from "../services/FakeApiService";
+import fakeApiService from "../services/FakeApiService.js";
 import Storage from "../services/Storage.js";
 
 class AboutComponent {
     constructor() {}
-    render(user) {
-        fakeApiService.login(user)
-            .then(response => response.json())
-            .then(data => Storage.setData('user', data))
-            .catch(error => console.error(error))
+    render() {
         let userInfo = [Storage.getData('user')]
+        console.log(userInfo)
+
         let aboutComponent = HTML(`
             <div class="about">
-                <div class="user-photo">
-                    <img src="./assets/images/main/online.png" alt="online">
-                    <img src="./assets/images/main/ellipse.png" alt="photo">
-                </div>
-                ${userInfo.map(({name, email, position, description, phone, address, organization}) => `
-                    <div class="user-data">
-                    <h2>${name}</h2>
-                    <h3>${position}</h3>
-                    <p>${description}</p>
-                 </div>
-                 <ul class="user-contacts">
-                    <li class="user-contacts__item">
-                    <h4>Email</h4>
-                    <span>${email}</span>
-                    </li>
-                    <li class="user-contacts__item">
-                    <h4>Phone</h4>
-                    <span>${phone}</span>
-                    </li>
-                    <li class="user-contacts__item">
-                    <h4>Address</h4>
-                    <span>${address}</span>
-                    </li>
-                    <li class="user-contacts__item">
-                    <h4>Organization</h4>
-                    <span>${organization}</span>
-                    </li>
-                </ul>
-                `).join('')}
+               <div class="user-photo">
+                <img src="./assets/images/main/online.png" alt="online">
+                <img src="./assets/images/main/ellipse.png" alt="photo">
+            </div>
+            ${userInfo.map(({name, email, position, description, phone, address, organization}) => `
+                <div class="user-data">
+                <h2>${name}</h2>
+                <h3>${position}</h3>
+                <p>${description}</p>
+             </div>
+             <ul class="user-contacts">
+                <li class="user-contacts__item">
+                <h4>Email</h4>
+                <span>${email}</span>
+                </li>
+                <li class="user-contacts__item">
+                <h4>Phone</h4>
+                <span>${phone}</span>
+                </li>
+                <li class="user-contacts__item">
+                <h4>Address</h4>
+                <span>${address}</span>
+                </li>
+                <li class="user-contacts__item">
+                <h4>Organization</h4>
+                <span>${organization}</span>
+                </li>
+            </ul>
+            `).join('')}
             </div>
         `)
         return aboutComponent
